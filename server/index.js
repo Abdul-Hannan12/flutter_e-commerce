e06a66b1abe8720f1cd185f08055b466b30bf650
Abdul Hannan <abdulhannan8540680@gmail.com>
2023-01-24 23:25:05 +0500

    added express app

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log("Backend Added");
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Connected at port ${PORT}`);
+});
