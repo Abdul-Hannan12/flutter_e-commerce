3a6b6b6d4b7c5e2e26fce22dc7bba30c45e7edc4
Abdul Hannan <abdulhannan8540680@gmail.com>
2023-01-25 12:04:45 +0500

    added .env and edited gitignore

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
+// IMPORTS FROM PACKAGES
 const express = require("express");
+const mongoose = require("mongoose");
 
+// IMPORTS FROM FILES
+const authRouter = require("./routes/auth");
+
+// INITIALIZATIONS
 const app = express();
-
 const PORT = 3000;
+const DB =
+  "mongodb+srv://abdulhannan:flutteramazon123@cluster0.hu7ex4c.mongodb.net/?retryWrites=true&w=majority";
+
+// MIDDLEWARES
+app.use(authRouter);
+
+// CONNECTIONS
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Connected at port ${PORT}`);
+  console.log(`Server listening at port ${PORT}`);
 });
