d3861c27479e387d6c7921779b6784ccdb2a38fa
Abdul Hannan <abdulhannan8540680@gmail.com>
2023-01-26 21:26:54 +0500

    added jwt token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const authRouter = express.Router();
@@ -33,14 +34,16 @@ authRouter.post("/api/signup", async (req, res) => {
 authRouter.post("/api/signin", async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = User({ email, password });
+    const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ msg: "Incorrect Email" });
     }
-    const isMatch = bcryptjs.compare(password, user.password);
+    const isMatch = await bcryptjs.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ msg: "Incorrect Password" });
     }
+    const token = jwt.sign({ id: user._id }, "secret");
+    res.json({ token, ...user._doc });
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
